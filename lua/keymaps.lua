47fb6b8c4c3fd4a743800e6f61f8881512e2367a
Peter DV <pdevroom@outlook.com>
2022-07-20 18:27:04 +1000

    Update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,38 +1,43 @@
-local function keymap(mode, keys, command, opts)
-	vim.api.nvim_set_keymap(mode, keys, command, opts)
-end
+-- local function keymap(mode, keys, command, opts)
+-- 	vim.api.nvim_set_keymap(mode, keys, command, opts)
+-- end
+local keymap = vim.keymap
 
 vim.g.mapleader = ' '
 
-keymap('n', '<c-_>', '<cmd>lua require\'commentor\'.comment()<CR>', {noremap = true})
+keymap.set('n', '<c-_>', 'gcc', {})
+
 -- Telescope
---keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>', {noremap = true})
-keymap('n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<CR>', {noremap = true})
--- keymap('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', {noremap = true})
--- keymap('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', {noremap = true})
+keymap.set('n', '<leader>ff', function() require("telescope.builtin").find_files() end)
+keymap.set('n', '<c-p>', function() require("telescope.builtin").find_files() end)
+keymap.set('n', '<leader>fg', function() require("telescope.builtin").live_grep() end)
+keymap.set('n', '<leader>fb', function() require("telescope.builtin").buffers() end)
  
-keymap('n', '<c-l>', '<c-w><c-l>', {noremap = true})
-keymap('n', '<c-h>', '<c-w><c-h>', {noremap = true})
-keymap('n', '<c-j>', '<c-w><c-j>', {noremap = true})
-keymap('n', '<c-k>', '<c-w><c-k>', {noremap = true})
+keymap.set('n', '<c-l>', '<c-w><c-l>')
+keymap.set('n', '<c-h>', '<c-w><c-h>')
+keymap.set('n', '<c-j>', '<c-w><c-j>')
+keymap.set('n', '<c-k>', '<c-w><c-k>')
 
 -- keymap('n', '<leader>`', '<cmd>vsplit | vertical resize 80 | terminal<CR>', {noremap = true})
 -- keymap('t', '<leader>`', [[<c-\><c-n><cmd>q<CR>]], {noremap = true})
-keymap('n', '<c-b>', '<cmd>NvimTreeToggle<CR>', {noremap = true})
+keymap.set('t', '<ESC>', [[<c-\><c-n>]])
+keymap.set('n', '<c-b>', '<cmd>NvimTreeToggle<CR>')
 
 -- keymap('n', '<leader>cd', '<cmd>cd %:h<CR>', {noremap = true})
 
 local which_key = require'which-key'
 which_key.register({
 	['<leader>c'] = { '<cmd>cd %:h<CR>', 'Change Working Directory' },
-	['<leader>`'] = { '<cmd>vsplit | vertical resize 80 | terminal<CR>', 'Open Terminal' },
+	['<leader>`'] = { '<cmd>vsplit<CR> <c-w><c-l> <cmd>vertical resize 80<CR> <cmd>terminal<CR>', 'Open Terminal' },
+	['<leader>x'] = { '<cmd>w<CR> <cmd>source %<CR>', 'Run current file' },
 	['<leader>f'] = {
 		name = 'Telescope',
 		g = { '<cmd>lua require("telescope.builtin").live_grep()<CR>', 'Live Grep' },
 		b = { '<cmd>lua require("telescope.builtin").buffers()<CR>', 'Open Buffers' },
+		h = { '<cmd>lua require("telescope.builtin").help_tags()<CR>', 'Grep Help' },
 	},
 })
 
 which_key.register({
-	['<leader>`'] = { [[<c-\><c-n><cmd>q<CR>]], 'Close Terminal' },
+	['`'] = { [[<c-\><c-n><cmd>q<CR>]], 'Close Terminal' },
 }, { mode = 't' })
